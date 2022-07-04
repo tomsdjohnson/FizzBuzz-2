4b74e39b3ff1a4863e70915bb0bdfcdcc6b9ba18
Nabee; Husain <nabeelhusain03@gmail.com>
2022-07-04 16:13:49 +0100

    Finished Part 2

--- FizzBuzzProject/main.py
+++ FizzBuzzProject/main.py
@@ -11,14 +11,38 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    for x in range (1, 101):
-        if x % 3 == 0 and x % 5 == 0:
-            print("FizzBuzz")
-        elif x % 3 == 0:
-            print("Fizz")
-        elif x % 5 == 0:
-            print("Buzz")
-        else:
-            print(x)
+    for x in range (200, 260):
+        out_string = ""
+        if x % 3 == 0:
+            out_string += "Fizz"
+        if x % 5 == 0:
+            out_string += "Buzz"
+        if x % 7 == 0:
+            out_string += "Bang"
+        if x % 11 == 0:
+            out_string = "Bong"
+
+        if x % 13 == 0:
+            index = out_string.find('B')
+            if index == -1:
+                out_string += "Fezz"
+            else:
+                out_string = out_string[:index] + "Fezz" + out_string[index:]
+
+        if x % 17 == 0:
+            names = []
+            if len(out_string) > 4:
+                while len(out_string) != 0:
+                    names.append(out_string[-4:])
+                    out_string = out_string[-4:]
+
+                names.reverse()
+                for name in names:
+                    out_string += name
+
+        if out_string == "":
+            out_string += str(x)
+
+        print(out_string)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
